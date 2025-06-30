84c107fe072004805cb7fbddfd7ff1d8aaf6f685
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-06-30 16:57:57 -0300

    Criando endpoint para os componentes de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { HomeComponent } from './modules/home/home.component';
+import { RegisterComponent } from './modules/register/register.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'home',component: HomeComponent
+    },
+    {
+        path: 'register', component: RegisterComponent
     }
 ];
