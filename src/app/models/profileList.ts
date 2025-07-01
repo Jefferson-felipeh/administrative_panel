aef6723b5b7ddf76e502bec087c3a5838029511c
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-01 17:06:37 -0300

    Dto para profiles

---
+++ src/app/models/profileList.ts
@@ -0,0 +1,9 @@
+export interface ProfileListInterface {
+    createdAt: string
+    id: string
+    label: string
+    order: number
+    path: string
+    permission: string
+    updatedAt: string
+}
