fb44725615f7ede2d54aff4758197144af95f00a
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-01 17:07:54 -0300

    Componente de permissão

---
+++ src/app/modules/permissions/permissions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'permissions-component',
+    imports: [],
+    templateUrl: './permissions.component.html',
+    standalone: true
+})
+export class PermissionsComponent{}
