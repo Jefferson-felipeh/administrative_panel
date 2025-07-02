035cb3085a1348702f6ac897152294945640e75d
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-02 17:06:44 -0300

    Definindo os campos do template de menu

--- src/app/modules/menus/menus.component.ts
+++ src/app/modules/menus/menus.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from "@angular/core";
-import { Menu } from "../../models/signIn";
 import { FindMenus } from "../../shared/services/findMenus.service";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { CreateMenuComponent } from "./create-menu/create-menu.component";
 
 @Component({
     selector: 'menus-component',
-    imports: [CommonModule,FormsModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        CreateMenuComponent
+    ],
     templateUrl: './menus.component.html',
     providers: [FindMenus],
     standalone: true
 })
 export class MenusComponent implements OnInit{
     getMenus!:any;
-
+    obj_menu:{data:object} = {data: []}
+    openModal:boolean = false;
+    
     constructor(private findMenus:FindMenus){}
 
     ngOnInit(): void {
@@ -22,4 +28,6 @@ export class MenusComponent implements OnInit{
             console.log(this.getMenus);
         })
     }
+
+    openMenu = () => this.openModal = !this.openModal;
 }
