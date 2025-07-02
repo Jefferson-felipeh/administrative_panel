838350fd6cf719e20d63e21dd7630163b3516aee
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-02 17:06:19 -0300

    Componente que exibe a listagem de menu de acordo com sua lógica

---
+++ src/app/modules/menus/create-menu/create-menu.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CreateMenuService } from "./create-menu.service";
+import { Menu } from "../../../models/signIn";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: 'create-menu-component',
+    templateUrl: './create-menu.component.html',
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    providers: [CreateMenuService],
+    standalone: true
+})
+export class CreateMenuComponent implements OnInit{
+    @Input() obj_menu!:object;
+    @Output() close_menu = new EventEmitter<boolean>();
+
+    dataMenu:Partial<Menu> = {
+        label: '',
+        path: '',
+        icon: '',
+        permission: ''
+    };
+
+    constructor(
+        private createMenuService:CreateMenuService,
+        private router:Router
+    ){}
+
+    ngOnInit(): void {
+        console.log(this.obj_menu);
+    }
+
+    closeMenu = () => this.close_menu.emit(false);
+
+
+    createMenu(){
+        const createMenu = this.createMenuService.createMenu(this.dataMenu);
+
+        createMenu.subscribe({
+            next: (res => {
+                console.log(res);
+                this.router.navigate(['menus']);
+            }),
+            error: (error => {
+                console.log(error);
+            })
+        });
+    }
+}
