bfcee7eed1d233098f5975fc8887996bfd72ff46
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-03 17:02:47 -0300

    Retirando o locastorage

--- src/app/shared/services/findMenus.service.ts
+++ src/app/shared/services/findMenus.service.ts
@@ -9,21 +9,21 @@ export class FindMenus{
     menus$ = this.userSubject.asObservable();
 
     constructor(){
-        const saveStorage = localStorage.getItem('menus');//Criar um registro no locastorage;
-        if(saveStorage) this.userSubject.next(JSON.parse(saveStorage));
+        // const saveStorage = localStorage.getItem('menus');//Criar um registro no locastorage;
+        // if(saveStorage) this.userSubject.next(JSON.parse(saveStorage));
     }
 
-    setUser(menus:Menu){
-        localStorage.setItem('menus',JSON.stringify(menus));//Salva os dados nesse registro;
+    setMenus(menus:Menu){
+        // localStorage.setItem('menus',JSON.stringify(menus));//Salva os dados nesse registro;
         this.userSubject.next(menus);
     }
 
-    getUser(): object | null{
+    getMenu(): object | null{
         return this.userSubject.value;
     }
 
-    clearuser(){
-        localStorage.removeItem('menus');//Apaga os dados do localstorage referente a esse registro;
+    clearMenu(){
+        // localStorage.removeItem('menus');//Apaga os dados do localstorage referente a esse registro;
         this.userSubject.next(null);
     }
 }
