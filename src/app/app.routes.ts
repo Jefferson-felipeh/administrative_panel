65252b5a1ac0b30262177b5e4e5dc798ea7b93b7
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-04 16:05:00 -0300

    Adicionando as rotas como filhas da home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,24 +4,40 @@ import { HomeComponent } from './modules/home/home.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { MenusComponent } from './modules/menus/menus.component';
 import { ProfileComponent } from './modules/profile/profile.component';
+import { BodyComponent } from './modules/home/body/body.component';
 
 export const routes: Routes = [
     {
-        path: '',redirectTo:'login',pathMatch: 'full'
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
     },
     {
-        path: 'login', component: LoginComponent
+        path: 'login',
+        component: LoginComponent
     },
     {
-        path: 'home',component: HomeComponent
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {
+                path: 'menus', component: MenusComponent
+            },
+            {
+                path: 'perfil', component: ProfileComponent
+            },
+            {
+                path: 'body',
+                component: BodyComponent
+            },
+            {
+                path: '',
+                redirectTo: 'body',
+                pathMatch: 'full'
+            }
+        ]
     },
     {
         path: 'register', component: RegisterComponent
     },
-    {
-        path: 'menus', component: MenusComponent
-    },
-    {
-        path: 'perfil', component: ProfileComponent
-    }
 ];
