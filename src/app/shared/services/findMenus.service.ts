5dd0b1cbf1e89d37ea7f947cefac59b75b8c44a9
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-07 17:00:24 -0300

    salvando os dados do menu da requisição em um BehaviorSubject

--- src/app/shared/services/findMenus.service.ts
+++ src/app/shared/services/findMenus.service.ts
@@ -2,28 +2,16 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { Menu } from "../../models/signIn";
 
-
 @Injectable({providedIn: 'root'})
 export class FindMenus{
     private userSubject = new BehaviorSubject<Menu | null>(null);
     menus$ = this.userSubject.asObservable();
 
-    constructor(){
-        // const saveStorage = localStorage.getItem('menus');//Criar um registro no locastorage;
-        // if(saveStorage) this.userSubject.next(JSON.parse(saveStorage));
-    }
+    setMenus = (menus:Menu) => this.userSubject.next(menus);
 
-    setMenus(menus:Menu){
-        // localStorage.setItem('menus',JSON.stringify(menus));//Salva os dados nesse registro;
-        this.userSubject.next(menus);
-    }
-
-    getMenu(): object | null{
+    getMenu = (): object | null => {
         return this.userSubject.value;
     }
 
-    clearMenu(){
-        // localStorage.removeItem('menus');//Apaga os dados do localstorage referente a esse registro;
-        this.userSubject.next(null);
-    }
+    clearMenu = () => this.userSubject.next(null);
 }
