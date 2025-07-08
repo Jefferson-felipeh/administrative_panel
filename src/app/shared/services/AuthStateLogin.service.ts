3abadbadc2fd69c2631642389673ceeef366f3fe
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-08 16:43:56 -0300

    refatorando a lógica do behaviorSubject

--- src/app/shared/services/AuthStateLogin.service.ts
+++ src/app/shared/services/AuthStateLogin.service.ts
@@ -1,29 +1,27 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
-import { LoginUserModel } from "../../models/signIn";
-
 
 @Injectable({providedIn: 'root'})
 export class AuthStateLogin{
-    private userSubject = new BehaviorSubject<LoginUserModel | null>(null);
+    private userSubject = new BehaviorSubject<string | null>(null);
     user$ = this.userSubject.asObservable();
 
     constructor(){
-        const saveStorage = localStorage.getItem('user');//Criar um registro no locastorage;
-        if(saveStorage) this.userSubject.next(JSON.parse(saveStorage));
+        const saveStorage = localStorage.getItem('token');//Criar um registro no locastorage;
+        if(saveStorage) this.userSubject.next(saveStorage);
     }
 
-    setUser(user:LoginUserModel){
-        localStorage.setItem('user',JSON.stringify(user));//Salva os dados nesse registro;
-        this.userSubject.next(user);
+    setUser(token:string){
+        localStorage.setItem('token',token);//Salva os dados nesse registro;
+        this.userSubject.next(token);
     }
 
-    getUser(): object | null{
+    getUser(): string | null{
         return this.userSubject.value;
     }
 
     clearuser(){
-        localStorage.removeItem('user');//Apaga os dados do localstorage referente a esse registro;
+        localStorage.removeItem('token');//Apaga os dados do localstorage referente a esse registro;
         this.userSubject.next(null);
     }
 }
