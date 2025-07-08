a6ea00c3b5869446401e67b998cf8102b0186a53
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-08 16:42:51 -0300

    alterando o Dto do menu

---
+++ src/app/models/menu.ts
@@ -0,0 +1,11 @@
+export interface Menu{
+        active: boolean
+        createdAt: string
+        icon: string
+        id: string
+        label: string
+        ordem: null
+        path: string
+        permission: string
+        updatedAt: string
+};
