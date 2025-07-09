ef468cf76569dbe435c7715116a99cbcb97b8b51
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-09 17:09:03 -0300

    Refatorando o código

--- src/app/shared/services/findMenus.service.ts
+++ src/app/shared/services/findMenus.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
-import { Menu } from "../../models/signIn";
+import { Menu } from "../../models/menu";
 
 @Injectable({providedIn: 'root'})
 export class FindMenus{
-    private userSubject = new BehaviorSubject<Menu | null>(null);
+    private userSubject = new BehaviorSubject<Menu[] | null>(null);
     menus$ = this.userSubject.asObservable();
 
-    setMenus = (menus:Menu) => this.userSubject.next(menus);
+    setMenus = (menus:Menu[]) => this.userSubject.next(menus);
 
     getMenu = (): object | null => {
         return this.userSubject.value;
