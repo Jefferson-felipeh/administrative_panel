e3331543e785ae6086b01e50b76f2cd02f1c23bb
Jefferson Felipe <jeffersons@petribusa.com.br>
2025-07-14 17:01:52 -0300

    Refatorando o código

--- src/app/modules/permissions/permissions.component.ts
+++ src/app/modules/permissions/permissions.component.ts
@@ -7,3 +7,16 @@ import { Component } from "@angular/core";
     standalone: true
 })
 export class PermissionsComponent{}
+
+/**
+ * Eu irei reconstruir toda parte das regras do casbin dividindo em endpoints da API e endpoints da SPA.
+ * Para isso, irei adicionar mais parametros a Policy_definition do model do casbin para se adequar com as regras da entidade.
+ * Eu vou criar o parametro scope com valores API ou SPA.
+ * Irei criar um quinto parametro para definir o contexto;
+ * 
+ * Irei definir a segurança do frontend com base no(AuthGuard, Resolver, Interceptor e ngIf) além das regras de security do próprio backend;
+ * 
+ * Há dois componentes que estão fazendo requisições para buscar dados que o próprio Home já possue, logo mudar essa estratégia.
+ * È o componente Menus e o profile;
+ * 
+ */
